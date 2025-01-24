27b732fd4c6a3041993ee222944589952e4904e9
Michael Lee <mikecovlee@163.com>
2025-01-24 15:06:00 +0800

    update libucontext to v1.3.2

--- libucontext/arch/arm/defs.h
+++ libucontext/arch/arm/defs.h
@@ -2,8 +2,11 @@
 
 #define REG_SZ		(4)
 #define MCONTEXT_GREGS	(32)
+#define VFP_MAGIC_OFFSET (232)
+#define VFP_D8_OFFSET (304)
 
-#define TYPE(__proc)
+#define TYPE(__proc)	.type	__proc, %function;
+#define END(__proc)
 
 #define FETCH_LINKPTR(dest) \
 	asm("movs    %0, r4" : "=r" ((dest)))
